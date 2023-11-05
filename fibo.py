2e968bdb967951c150b25011db87bad18cb6f7c0
Anagha Rajput <111584300+Anagha8302@users.noreply.github.com>
2023-11-05 10:08:10 +0530

    fibo.py

--- fibo.py
+++ fibo.py
@@ -12,9 +12,23 @@ def iterative(n):
     for i in range(2, n):
         print(a + b,end=" ")
         a, b = b, a + b
-        
+
+def find_fibonacci_step_count(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        count = 1
+        while b < n:
+            a, b = b, a + b
+            count += 1
+        return count
+
 if __name__ == "__main__":
     n = int(input("Enter the nth number for series:"))
+    step_count = find_fibonacci_step_count(n)
     if n<=0:
         print("Please enter the positive integer")
     else:
@@ -24,3 +38,5 @@ if __name__ == "__main__":
         print()
         print("Fibonacci series with iteration:")
         iterative(n)
+        print()
+        print(f"Step count to reach n = {n}: {step_count}")
